bf538458c467bea4da289dcf814b33323bf403c9
Jvzqz13 <142378858+Jvzqz13@users.noreply.github.com>
2023-11-21 09:30:09 -0500

    Section 1 part B

---
+++ Section1/src/index.js
@@ -0,0 +1,7 @@
+//Section 1 Part A
+const myName = "Jorge";
+const favoriteLanguage =  "Python";
+const newFavoriteLanuage = "JavaScript";
+console.log(`My name is ${myName} and ${newFavoriteLanuage} is my favorite language.`);
+
+//Section 1 Part B
