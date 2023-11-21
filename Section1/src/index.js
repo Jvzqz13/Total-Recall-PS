856fd39525f915a78c5a636e07259520a79e31bf
Jvzqz13 <142378858+Jvzqz13@users.noreply.github.com>
2023-11-21 09:42:03 -0500

    section 1 part b

--- Section1/src/index.js
+++ Section1/src/index.js
@@ -5,3 +5,12 @@ const newFavoriteLanuage = "JavaScript";
 console.log(`My name is ${myName} and ${newFavoriteLanuage} is my favorite language.`);
 
 //Section 1 Part B
+let firstVariable = "Hello World";
+firstVariable = 12312312;
+
+let secondVariable = firstVariable;
+secondVariable = "string"
+console.log(firstVariable);
+let yourName = "Jorge";
+console.log(`Hello, my name is ${yourName}`);
+
